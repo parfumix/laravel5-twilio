08c549f0c19c560d0c735461780ad8fdd6bf62ba
Radu <radu.bordei89@gmail.com>
2015-02-09 10:03:00 +0200

    adding default twilio connection .

---
+++ src/config/configuration.php
@@ -0,0 +1,27 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | DEFAULT CONNECTION
+    |--------------------------------------------------------------------------
+    |
+    | Your Twilio default connections which will be used.
+    |
+    */
+
+    'default' => 'twilio',
+
+    'connections' => array(
+
+        'twilio' => array(
+
+            'sid'    => '',
+
+            'token'  => '',
+
+            'from'   => '',
+        )
+    )
+);
