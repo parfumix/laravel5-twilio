30c89aa085eea357932dc61c35cf3c71621a6e79
Radu <radu.bordei89@gmail.com>
2015-02-09 11:39:02 +0200

    adding call function .

--- src/Twilio/Twilio.php
+++ src/Twilio/Twilio.php
@@ -1,5 +1,7 @@
 <?php namespace Twilio;
 
+use Services_Twilio;
+
 class Twilio {
 
     /**
@@ -28,5 +30,18 @@ class Twilio {
         $this->token = $token;
         $this->from = $from;
         $this->sslVerify = $sslVerify;
+    }
+
+    /**
+     * Call an specific phone by number
+     *
+     * @param $phone
+     * @param $twiml
+     * @return mixed
+     */
+    public function call($phone, $twiml = '') {
+        $client = new Services_Twilio($this->sid, $this->token);
+
+        return $client->account->calls->create($this->from, $phone, $twiml);
     }
 }
